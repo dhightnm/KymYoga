a8cb4375fd4a16498e632264f1acab31df0986e1
Devin Hight <dhightnm@gmail.com>
2021-11-28 12:09:36 -0800

    heroku app branch

--- server/index.js
+++ server/index.js
@@ -1,26 +1,12 @@
 const path = require('path');
 const express = require('express');
-
-
 const app = express();
-const port = 3001;
-
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Las Vegas!" });
-});
-
-app.get("/api/net", (req, res) => {
-  res.json({ message: "Hello from server NEW!" });
-});
-// console.log(port)
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+   console.log('Server is up!');
+});
